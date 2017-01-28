b244772ffbdca8694c9df2d51a2d7fb8eccc5fb2
Syed Tajammul Hussain <tajammul81@gmail.com>
2017-01-28 02:13:30 +0100

    Search of players are done

--- src/Chelsea_Vs_Manchester_United/League.java
+++ src/Chelsea_Vs_Manchester_United/League.java
@@ -27,7 +27,7 @@ public class League {
 
         team2.players = new Player[3];
         team2.players[0] = new Player();
-        team2.players[0].PlayerName = "Ibrahimivic";
+        team2.players[0].PlayerName = "Zlatan Ibrahimivic";
         team2.players[1] = new Player();
         team2.players[1].PlayerName = "Juan Mata";
         team2.players[2] = new Player();
@@ -45,33 +45,57 @@ public class League {
             System.out.println(The_Players.PlayerName);
         }
 
-        Game game= new Game();
-        game.Away_team= team1;
-        game.Home_team= team2;
-        Goal goal1= new Goal();
-        goal1.Goal_Time=0.5;
-        goal1.The_Players= game.Away_team.players[1];
-        goal1.The_Team=game.Away_team;
+        Game game = new Game();
+        game.Away_team = team1;
+        game.Home_team = team2;
+        Goal goal1 = new Goal();
+        goal1.Goal_Time = 0.5;
+        goal1.The_Players = game.Away_team.players[1];
+        goal1.The_Team = game.Away_team;
         Goal goal2 = new Goal();
         goal2.The_Players = game.Away_team.players[0];
-        goal2.The_Team= game.Away_team;
-        goal2.Goal_Time= 15;
+        goal2.The_Team = game.Away_team;
+        goal2.Goal_Time = 15;
         Goal goal3 = new Goal();
         goal3.The_Players = game.Away_team.players[1];
         goal3.The_Team = game.Away_team;
         goal3.Goal_Time = 67;
-        Goal [] the_goals= {goal1, goal2, goal3};
-        game.goals= the_goals;
+        Goal[] the_goals = {goal1, goal2, goal3};
+        game.goals = the_goals;
         System.out.println("");
         System.out.println("Goals:");
-        for(Goal g : game.goals) {
-            System.out.println("Goal Scored after " + g.Goal_Time + " mins by " + g.The_Players.PlayerName + " of the " +
-                    g.The_Team.TeamName);
+        for (Goal g : game.goals) {
+            System.out.println("Goal Scored after " + g.Goal_Time + " mins by " + g.The_Players.PlayerName + " of the "
+                    + g.The_Team.TeamName);
         }
 
 //        System.out.println("Goal Scored after " + goal1.Goal_Time + " mins by " + player1.PlayerName + " of the " + team1.TeamName);
 //        System.out.println("Goal Scored after " + goal2.Goal_Time + " mins by " + player1.PlayerName + " of the " + team1.TeamName);
 //        System.out.println("Goal Scored after " + goal3.Goal_Time + " mins by " + team2.players[1].PlayerName + " of the " + team2.TeamName);
+        System.out.println(" ");
+        for (Player The_Players : team1.players) {
+
+            String lastName = The_Players.PlayerName.substring(The_Players.PlayerName.indexOf(" "),
+                    The_Players.PlayerName.length());
+            
+            //System.out.println(FullName);
+            if (lastName.matches(".*Co.*")) {
+                System.out.println("Found " + The_Players.PlayerName);
+
+            }
+
+        }
+        //Player[] name= team2.players;
+        for (Player The_Players : team2.players) {
+            String lastName = The_Players.PlayerName.substring(The_Players.PlayerName.indexOf(" "),
+                    The_Players.PlayerName.length());
+
+            String firstName = The_Players.PlayerName.substring(The_Players.PlayerName.indexOf(The_Players.PlayerName),
+                    The_Players.PlayerName.indexOf(lastName));
+            String FullName = lastName + ", " + firstName;
+            System.out.println(FullName);
+
+        }
 
     }
 }
